dd16108f3144bdfb8b1dacd60b9fa5aab3679554
Shawn Allen <shawn.allen@github.com>
2019-09-19 15:15:21 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const {spawnSync} = require('child_process')
+const getOriginURL = require('remote-origin-url').sync
+const parseGitURL = require('git-url-parse')
+const Table = require('./table')
+
+module.exports = {
+  getHeadRef,
+  getRepoURL,
+  stripCwd,
+  Table
+}
+
+function getHeadRef() {
+  const {stdout} = spawnSync('git', ['show-ref', '-s', 'HEAD'], {encoding: 'utf8'})
+  return stdout.trim()
+}
+
+function getRepoURL() {
+  const remoteURL = getOriginURL()
+  const {name, owner} = parseGitURL(remoteURL)
+  return `https://github.com/${name}/${owner}`
+}
+
+function stripCwd(path, cwd) {
+  const prefix = cwd || process.cwd()
+  return path.startsWith(prefix) ? path.substr(prefix.length + 1) : path
+}
---
+++ table.js
@@ -0,0 +1,44 @@
+module.exports = class Table {
+  constructor(options = {}) {
+    const {columns = [], delimiter = '\t', lineDelimiter = '\n', beforeLine = '', afterLine = ''} = options
+    Object.assign(this, {
+      columns,
+      delimiter,
+      lineDelimiter,
+      beforeLine,
+      afterLine
+    })
+  }
+
+  header() {
+    return this.columns.map(column => this.heading(column)).join(this.delimiter)
+  }
+
+  heading(column) {
+    return typeof column === 'string' ? column : column.title
+  }
+
+  row(row) {
+    const cells = this.columns.map(column => this.cell(column, row))
+    const content = cells.map(cell => this.escape(cell)).join(this.delimiter)
+    return `${this.beforeLine}${content}${this.afterLine}`
+  }
+
+  cell(column, row) {
+    if (typeof column === 'string') {
+      return row[column]
+    } else if (typeof column.key === 'string') {
+      return row[column.key]
+    } else if (typeof column.format === 'function') {
+      return column.format(row, column)
+    }
+  }
+
+  escape(str) {
+    return str.replace(new RegExp(`${this.delimiter}`, 'g'), ' ')
+  }
+
+  format(rows) {
+    return [this.header(), ...rows.map(row => this.row(row))].join(this.lineDelimiter)
+  }
+}
